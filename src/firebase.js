7f6ea223888f86fa24801e3d96f7583530808b5c
Nika Yakubova <lilit666an@gmail.com>
2021-04-27 20:00:00 +0300

    Add react-router-dom

---
+++ src/firebase.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCz358gvjEk-BDk1L0rub2acuqBGPfZQq8",
+    authDomain: "slack-clone-800c5.firebaseapp.com",
+    projectId: "slack-clone-800c5",
+    storageBucket: "slack-clone-800c5.appspot.com",
+    messagingSenderId: "1075851486430",
+    appId: "1:1075851486430:web:f8a8d4a3a6bd4bd25a4e4f"
+}
+
+
+
